ae7fb67252d81f0b70c015dee84e6db4cc2d7b7c
Nahiyan Kamal <noyanayihan@gmail.com>
2025-05-30 23:26:41 +0200

    test message

--- index.ts
+++ index.ts
@@ -4,21 +4,28 @@ import * as fs from 'fs';
 
 import printTableFromInp from './src/service';
 
-const program = new Command();
+export function runCLI(argv: string[] = process.argv) {
+  const program = new Command();
 
-program
-  .option('-i, --input <value>', 'input string')
-  .option('-s, --stdin', 'read input from stdin')
-  .option('-t, --tableOptions <value>', 'table options in JSON format')
-  .parse(process.argv);
+  program
+    .option('-i, --input <value>', 'input string')
+    .option('-s, --stdin', 'read input from stdin')
+    .option('-t, --tableOptions <value>', 'table options in JSON format')
+    .parse(argv);
 
-const options = program.opts();
+  const options = program.opts();
 
-if (options.input) {
-  console.log('program.input', options.input);
-  printTableFromInp(options.input, options.tableOptions);
-} else if (options.stdin) {
-  printTableFromInp(fs.readFileSync(0).toString(), options.tableOptions);
-} else {
-  console.log('Error: Cant detect input option');
+  if (options.input) {
+    console.log('program.input', options.input);
+    printTableFromInp(options.input, options.tableOptions);
+  } else if (options.stdin) {
+    printTableFromInp(fs.readFileSync(0).toString(), options.tableOptions);
+  } else {
+    console.log('Error: Cant detect input option');
+  }
+}
+
+// Run CLI if this file is executed directly
+if (require.main === module) {
+  runCLI();
 }
