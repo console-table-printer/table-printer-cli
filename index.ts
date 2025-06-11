44bba3b50de8c8657df7e72622692e47b9de38ac
Nahiyan Kamal <noyanayihan@gmail.com>
2025-06-11 09:52:15 +0200

    fix: Addign version of the package

--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import * as fs from 'fs';
+import * as path from 'path';
 
 import printTableFromInp from './src/service';
+
+// Read package.json to get version
+const packageJson = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')
+);
 
 export function runCLI(argv: string[] = process.argv) {
   const program = new Command();
 
   program
+    .version(packageJson.version, '-v, --version', 'output the current version')
     .option('-i, --input <value>', 'input string')
     .option('-s, --stdin', 'read input from stdin')
     .option('-t, --tableOptions <value>', 'table options in JSON format')
